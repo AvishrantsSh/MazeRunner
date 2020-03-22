75a0fce587336984a784ba69b08220a1f4865b28
Avishrant Sharma <avishrants@gmail.com>
2020-03-22 17:37:20 +0530

    commit

--- MazeRunner.py
+++ MazeRunner.py
@@ -6,9 +6,9 @@ def normalise(img, dim):
         for y in range(0,dim[1]):
             if np.any(img[x][y] != 255) and np.any(img[x][y] != 0):
                 img[x][y] = [0,0,0]
+
 img = cv2.imread("maze.png")
-dim = img.shape
-normalise(img,dim)
+normalise(img,img.shape)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)

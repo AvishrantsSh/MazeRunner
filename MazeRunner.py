e17d16f2d4c4b0154f5630630985140b7863ffe3
Avishrant Sharma <avishrants@gmail.com>
2020-03-22 17:45:00 +0530

    cc

--- MazeRunner.py
+++ MazeRunner.py
@@ -7,8 +7,10 @@ def normalise(img, dim):
             if np.any(img[x][y] != 255) and np.any(img[x][y] != 0):
                 img[x][y] = [0,0,0]
 
-img = cv2.imread("maze.png")
-normalise(img,img.shape)
+#adjust location as per your convenience
+img = cv2.imread("/home/avishrant/GitRepo/MazeRunner/maze.png")
+dim = img.shape
+normalise(img,dim)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)

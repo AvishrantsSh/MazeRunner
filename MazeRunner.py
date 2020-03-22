e3defc04539ff7d21a221913be0e1b8cdbcfc09e
Avishrant Sharma <avishrants@gmail.com>
2020-03-22 19:18:02 +0530

    e

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,15 +1,19 @@
+#Applicable Only for square-cell Mazes
+
 import cv2
 import numpy as np
 
 def normalise(img, dim):
     for x in range(0,dim[0]):
         for y in range(0,dim[1]):
-            if np.any(img[x][y] != 255) and np.any(img[x][y] != 0):
+            if np.any(img[x][y] != 255):
                 img[x][y] = [0,0,0]
-    spixel(img,dim)
+    sarr(img,dim)
 
-def spixel(img, dim):
+def sarr(img, dim):
     print("Under Development")
+    
+
 
 #adjust location as per your convenience
 img = cv2.imread("/home/avishrant/GitRepo/MazeRunner/maze.png")

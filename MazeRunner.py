2b2bd097e152335461dded8b99aaaec8172786ea
Avishrant Sharma <avishrants@gmail.com>
2020-03-22 19:48:00 +0530

    hj

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,8 +1,9 @@
-#Applicable Only for square-cell Mazes
+#Applicable Only for Square-Cell Mazes                                                                         Mazes
 
 import cv2
 import numpy as np
 
+mh , mw = 100 , 100
 def normalise(img, dim):
     for x in range(0,dim[0]):
         for y in range(0,dim[1]):
@@ -10,9 +11,39 @@ def normalise(img, dim):
                 img[x][y] = [0,0,0]
     sarr(img,dim)
 
+def getmin(img , dim):
+    global mw,mh
+    for x in range(1,dim[0] - 1):
+        countw = 0
+        st = True
+        for y in range(1,dim[1] - 1):
+            if np.all(img[x][y] == 255):
+                countw += 1
+                st = False
+            else:
+                if st == False:
+                    break
+        if mw > countw and countw != 0:
+            mw = countw
+
+    for y in range(1,dim[1] - 1):
+        counth = 0
+        st = True
+        for x in range(1,dim[0] - 1):
+            if np.all(img[x][y] == 0):
+                counth += 1
+                st = False
+            else:
+                if st == False:
+                    break
+
+        if mh > counth and counth != 0:
+            mh = counth
+
 def sarr(img, dim):
+    getmin(img,dim)
     print("Under Development")
-    
+    print(mh,mw)
 
 
 #adjust location as per your convenience

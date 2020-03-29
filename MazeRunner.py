24786e3e33345dd9d690ed0aeddba7bf84897019
Avishrant Sharma <avishrants@gmail.com>
2020-03-29 20:20:50 +0530

    Added Library

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,198 +1,25 @@
 #Applicable Only for Rectangle-Cell Mazes
-
-import cv2,sys,time
-import numpy as np
-import easygui
+import cv2,time
+from sys import setrecursionlimit
+from easygui import fileopenbox
+from lib_maze import solver
 #from termcolor import colored
-sys.setrecursionlimit(10**6)
-
-#Change the indentation fo solution_path here
-#sol_ind = 1
-
-#[b,g,r] format
-sol_clr = [255,0,0]
-path = 255
-border = 0
-mh,mw = 50,0
-sr,er,sc,ec = 0,0,0,0
-arr = []
-res = []
-state = False
-def normalise():
-    global img,dim
-    ##This Function Needs Some serious shit of work            
-    print("> Normalised")
-    getbounds()
-    rect_size()
-
-def getbounds():
-    global sc,sr,ec,er,img,dim
-    for x in range(0,dim[0]):
-        st = True
-        for y in range(0,dim[1]):
-            if np.all(img[x][y] == 0):
-                sc = y
-                sr = x
-                st = False
-                break
-        if st == False:
-            break
-
-    for x in range(dim[0],0,-1):
-        st = True
-        for y in range(dim[1],0,-1):
-            if np.all(img[x-1][y-1] == 0):
-                ec = y
-                er = x
-                st = False
-                break
-        if st == False:
-            break
-    print("> Got Boundaries")
-
-def rect_size():
-    global mw,mh,sc,sr,ec,er,img
-    index = 0
-    for y in range(sc,ec):
-        x = sr
-        if np.all(img[x][y] == 255):
-            if mw == 0:
-                index = y
-            mw += 1
-
-    counth = 0
-    for x in range(sr,er):
-        if np.all(img[x][index-1] == 0):
-            counth += 1
-        else:
-            if mh > counth and counth != 0:
-                mh = counth
-            counth = 0
-    
-    print("> Got Maze Struct. Info")
-
-def binarray():
-    global arr,img,dim
-    xt = 0
-    yt = 0
-    x = sc
-    while x < ec:
-        tmp=[]
-        y = sr         
-        while y < er:
-            if np.all(img[x][y] == 0):
-                tmp.append(0)
-            else:
-                tmp.append(1)
-        
-            y += (mh if yt % 2 == 0 else mw)
-            yt += 1
-        
-        x += (mh if xt % 2 == 0 else mw)
-        xt += 1
-        arr.append(tmp) 
-
-    coord = arr[0].index(1)
-    res.append([0,coord])
-    print("> Constructed Array")
-    getsolindex(0,coord,0,0)
-
-def getsolindex(row,col,prow,pcol):
-    global res,arr,state
-    if row == len(arr) -  1:
-        state = True
-        print("> Solution Found")
-        return
-    
-    try:
-            
-        if arr[row+1][col] == 1 and (prow != row+1 or pcol != col) and state == False:
-            res.append([row+1,col])
-            getsolindex(row+1,col,row,col)
-        
-        if arr[row][col-1] == 1 and (prow != row or pcol != col-1) and state == False:
-            res.append([row,col-1])
-            getsolindex(row,col-1,row,col)
-                    
-        if arr[row][col+1] == 1 and (prow != row or pcol != col+1) and state == False:
-            res.append([row,col+1])
-            getsolindex(row,col+1,row,col)
-        
-        if arr[row-1][col] == 1 and (prow != row-1 or pcol != col) and state == False:
-            res.append([row-1,col])
-            getsolindex(row-1,col,row,col)
-            
-        if state == False:
-            res = res[:-1]
-        
-    except:
-        print("Oops...No Solution Found")
-
-def construct():
-    binarray()
-    print("> Constructing...")
-    global sc,sr,ec,er,res,mh,mw,img,sol_ind
-    img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-    x = sc
-    xt = 0
-    while x < ec:
-        y = sr
-        yt = 0
-        while y < er:
-            if [xt,yt] in res:
-                h,k = 0,0
-                k = mh + y if yt % 2 == 0 else mw + y
-                h = mh + x if xt % 2 == 0 else mw + x
-               
-                img[x:h,y:k] = sol_clr    
-            
-            y += (mh if yt % 2 == 0 else mw)
-            yt += 1
-        
-        x += (mh if xt % 2 == 0 else mw)
-        xt += 1
-    ##This Part is Under Development and might not work perfectly..Result won't be affected if uncommented
-    
-    # for h in range(sc,ec):
-    #     for k in range(sr,er):
-    #         if np.all(img[h][k] == 0):
-    #             try:
-    #                 if np.all(img[h-sol_ind][k] == sol_clr):
-    #                     img[h-sol_ind][k] = path
-
-    #                 if np.all(img[h+sol_ind][k] == sol_clr):
-    #                     img[h+sol_ind][k] = path
-
-    #                 if np.all(img[h][k-sol_ind] == sol_clr):
-    #                     img[h][k-sol_ind] = path
-
-    #                 if np.all(img[h][k+sol_ind] == sol_clr):
-    #                     img[h][k+sol_ind] = path
-                        
-    #             except:
-    #                 logging.error("401: Index Out of Bounds")
-    print("Showing Result.")
+setrecursionlimit(10**6)
 
 #adjust location as per your convenience
-
-img_path = easygui.fileopenbox()
 try:
+    img_path = fileopenbox()
     origimg = cv2.imread(img_path)
     (thresh, img) = cv2.threshold(cv2.cvtColor(origimg, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
-    start = time.time()
 
-    dim = img.shape
+    sol = solver(img)
+    start = time.time()
     print("Under Development")
 
     print("The process may take time, according to complexity and size of maze")
-    normalise()
-    construct()
 
-    ##Uncomment to see Image Details
-    #print("Minimum Edge :",mh,"\n","Minimum Path :",mw,"\n","Starting Col :",sc,"\n","Starting Row :",sr,"\n","Ending Col :",ec,"\n","Ending Row :",er,sep='')
-    ##Uncomment to see the binary-array
-    # for x in arr:
-    #     print(*x , sep = ' ')
+    sol.normalise()
+    img = sol.construct()
 
     end = time.time()
     print("Execution Time :",end-start,"s")
@@ -201,4 +28,4 @@ try:
     cv2.destroyAllWindows()
 
 except:
-    print("Something Went Wrong")
+    print("Oops Something Is wrong")
---
+++ lib_maze.py
@@ -0,0 +1,142 @@
+import cv2
+import numpy as np
+class solver(object):
+    def __init__(self,img):
+        self.img = img
+        self.dim = img.shape
+        self.sol_clr = [255,0,0]
+        self.clr = [0,255]
+        self.mazeinf = [0,0,0,0,50,0]
+        self.arr = []
+        self.res = []
+        self.state = False
+
+    def normalise(self):
+        ##This Function Needs Some serious shit of work            
+        print("> Normalised")
+        self.getbounds()
+        self.rect_size()
+
+    def getbounds(self):
+        for x in range(0,self.dim[0]):
+            st = True
+            for y in range(0,self.dim[1]):
+                if np.all(self.img[x][y] == 0):
+                    self.mazeinf[0] = y
+                    self.mazeinf[2] = x
+                    st = False
+                    break
+            if st == False:
+                break
+
+        for x in range(self.dim[0],0,-1):
+            st = True
+            for y in range(self.dim[1],0,-1):
+                if np.all(self.img[x-1][y-1] == 0):
+                    self.mazeinf[1] = y
+                    self.mazeinf[3] = x
+                    st = False
+                    break
+            if st == False:
+                break
+        print("> Got Boundaries")
+
+    def rect_size(self):
+        index = 0
+        for y in range(self.mazeinf[0],self.mazeinf[1]):
+            x = self.mazeinf[2]
+            if np.all(self.img[x][y] == 255):
+                if self.mazeinf[5] == 0:
+                    index = y
+                self.mazeinf[5] += 1
+
+        counth = 0
+        for x in range(self.mazeinf[2],self.mazeinf[3]):
+            if np.all(self.img[x][index-1] == 0):
+                counth += 1
+            else:
+                if self.mazeinf[4] > counth and counth != 0:
+                    self.mazeinf[4] = counth
+                counth = 0
+        
+        print("> Got Maze Struct. Info")
+
+    def binarray(self):
+        xt = 0
+        yt = 0
+        x = self.mazeinf[0]
+        while x < self.mazeinf[1]:
+            tmp=[]
+            y = self.mazeinf[2]         
+            while y < self.mazeinf[3]:
+                if np.all(self.img[x][y] == 0):
+                    tmp.append(0)
+                else:
+                    tmp.append(1)
+            
+                y += (self.mazeinf[4] if yt % 2 == 0 else self.mazeinf[5])
+                yt += 1
+            
+            x += (self.mazeinf[4] if xt % 2 == 0 else self.mazeinf[5])
+            xt += 1
+            self.arr.append(tmp) 
+
+        coord = self.arr[0].index(1)
+        self.res.append([0,coord])
+        print("> Constructed Array")
+        self.getsolindex(0,coord,0,0)
+
+    def getsolindex(self,row,col,prow,pcol):
+        if row == len(self.arr) -  1:
+            self.state = True
+            print("> Solution Found")
+            return
+        
+        try:
+                
+            if self.arr[row+1][col] == 1 and (prow != row+1 or pcol != col) and self.state == False:
+                self.res.append([row+1,col])
+                self.getsolindex(row+1,col,row,col)
+            
+            if self.arr[row][col-1] == 1 and (prow != row or pcol != col-1) and self.state == False:
+                self.res.append([row,col-1])
+                self.getsolindex(row,col-1,row,col)
+                        
+            if self.arr[row][col+1] == 1 and (prow != row or pcol != col+1) and self.state == False:
+                self.res.append([row,col+1])
+                self.getsolindex(row,col+1,row,col)
+            
+            if self.arr[row-1][col] == 1 and (prow != row-1 or pcol != col) and self.state == False:
+                self.res.append([row-1,col])
+                self.getsolindex(row-1,col,row,col)
+                
+            if self.state == False:
+                self.res = self.res[:-1]
+        except:
+            print("Oops...No Solution Found")
+
+    def construct(self):
+        self.binarray()
+        print("> Constructing...")
+        self.img = cv2.cvtColor(self.img,cv2.COLOR_GRAY2RGB)
+        x = self.mazeinf[0]
+        xt = 0
+        while x < self.mazeinf[1]:
+            y = self.mazeinf[2]
+            yt = 0
+            while y < self.mazeinf[3]:
+                if [xt,yt] in self.res:
+                    h,k = 0,0
+                    k = self.mazeinf[4] + y if yt % 2 == 0 else self.mazeinf[5] + y
+                    h = self.mazeinf[4] + x if xt % 2 == 0 else self.mazeinf[5] + x
+                
+                    self.img[x:h,y:k] = self.sol_clr    
+                
+                y += (self.mazeinf[4] if yt % 2 == 0 else self.mazeinf[5])
+                yt += 1
+            
+            x += (self.mazeinf[4] if xt % 2 == 0 else self.mazeinf[5])
+            xt += 1
+        
+        print("Showing Result.")
+        return self.img
